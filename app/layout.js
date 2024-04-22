21fb6a3fa17fb0df9b12b292265f5457ac0ad4c5
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-04-22 23:56:01 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Portfolio of Dhairya Luthra - Software Developer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "Portfolio of Dhairya Luthra - Software Developer",
 };
 
 export default function RootLayout({ children }) {
